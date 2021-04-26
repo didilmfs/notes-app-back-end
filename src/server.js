522beb00781a11bb0a64f910c60a2048ef233ec2
Fadhil <didil.mfs@gmail.com>
2021-04-26 16:40:14 +0700

    fix bugs host value to private ipv4 addr

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '54.210.156.69',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.16.98',
     routes: {
         cors: {
           origin: ['*'],
